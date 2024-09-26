ce287a4fd448df51ab29962e0af7d2915355f08c
Oscar Alshammar <oscar.alshammar@gmail.com>
2024-09-26 10:00:01 +0200

    Slagsmål

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text.Json;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        
+        GameManager.LoadKillCount();
         GameManager gameManager = new GameManager();
         gameManager.StartGame();
     }
@@ -14,30 +17,25 @@ class Program
 class GameManager
 {
     private string playerName = "Spelare";
-    private int playerHp = 0;
+    private int playerHp = 100;
     private int playerMaxHp = 100;
     private int playerDamage;
 
-    private int enemyHp = 0;
+    private int enemyHp = 100;
     private int enemyDamage;
     private string enemyName = "Enemy";
 
+    private static int killCount;
+    private static string killCountFilePath = "killcount.json";
+
     public void StartGame()
     {
-        
-        string[] enemyNames;
-        try
-        {
-            enemyNames = File.ReadAllLines(@"./enemy.txt");
-            int randomIndex = new Random().Next(0, enemyNames.Length);
-            enemyName = enemyNames[randomIndex];
+        enemyName = LoadRandomEnemy();
+
+        if(enemyName == "Ronaldo"){
+            enemyHp = 1000;
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message + "\n");
-            enemyName = "Garlock";
-        }
-        
+        System.Console.WriteLine($"kills: {killCount} \n");
         Console.WriteLine("SPACE för attack \n");
         Console.WriteLine("Klicka en knapp för att starta!");
         Console.ReadKey();
@@ -49,7 +47,7 @@ class GameManager
             RunGameLoop();
             Console.WriteLine("Vill du spela igen? (J/N)");
             ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true);
-            if(keyInfo.Key == ConsoleKey.J){
+            if(keyInfo.Key == ConsoleKey.J || keyInfo.Key == ConsoleKey.Spacebar){
                 playerHp = playerMaxHp;
                 enemyHp = 100;
             }
@@ -57,6 +55,60 @@ class GameManager
                 break;
             }
         } 
+    }
+
+    private string LoadRandomEnemy(){
+        string[] enemyNames;
+        try
+        {
+            enemyNames = File.ReadAllLines(@"./enemy.txt");
+            int randomIndex = new Random().Next(0, enemyNames.Length);
+            return enemyNames[randomIndex];
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message + "\n");
+            return "Garlock";
+        }
+    }
+
+    private static void AddKillCount()
+    {
+        killCount++;
+        SaveKillCount();
+    }
+
+    private static void SaveKillCount() {
+        try{
+            var data = new Dictionary<string, int> { { "Kills", killCount} };
+            string json = System.Text.Json.JsonSerializer.Serialize(data);
+            File.WriteAllText("killcount.json", json);
+        }catch(Exception ex){
+            Console.WriteLine("kunde ispara killcount: " + ex.Message);
+        }
+    }
+
+    public static void LoadKillCount(){
+        try
+        {
+            if (File.Exists(killCountFilePath))
+            {
+                string json = File.ReadAllText(killCountFilePath);
+                
+                var data = JsonSerializer.Deserialize<Dictionary<string, int>>(json);
+                killCount = data?["Kills"] ?? 0;
+            }
+            else
+            {
+                killCount = 0;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error reading kill count: {ex.Message}");
+            killCount = 0;
+        }
+
     }
 
     private void AskName()
@@ -79,6 +131,7 @@ class GameManager
         while (playerHp > 0 && enemyHp > 0)
         {
             Console.WriteLine($"Du har {playerHp} hp kvar \n{enemyName} har {enemyHp} hp kvar");
+            System.Console.WriteLine($"\n\nkills: {killCount}");
 
             ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true);
             Console.Clear();
@@ -125,6 +178,7 @@ class GameManager
 
         if(playerHp == enemyHp) {
             Console.WriteLine("Det blev oavgjort!"); 
+            AddKillCount();
             return;
         }
 
@@ -132,6 +186,7 @@ class GameManager
             Console.WriteLine("Du förlorade!");
         } else {
             Console.WriteLine("Du vann!");
+            AddKillCount();
         }
     }
 }
